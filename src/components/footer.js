3f7b8b34ecda92e10a543054f5586b5de3760802
Daisy Tan <daisytansgbk@gmail.com>
2022-03-25 11:32:53 +0800

    footer change

--- src/components/footer.js
+++ src/components/footer.js
@@ -7,11 +7,7 @@ const Footer = () => (
     <div class="footer">
     
     <strong>Copyright © 2022 All rights reserved</strong>
-    <p>
-          <a href="#" className="fa fa-facebook"></a>
-          <a href="#" className="fa fa-twitter"></a>
-          <a href="#" className="fa fa-instagram"></a>  
-    </p> 
+    
   </div>
   </footer>
 );
